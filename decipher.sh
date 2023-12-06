8f58109ee6600f800f275fef783b229d4902bdb2
skqhfla <83075908+skqhfla@users.noreply.github.com>
2023-12-07 06:09:15 +0900

    Update decipher.sh

--- decipher.sh
+++ decipher.sh
@@ -5,9 +5,9 @@ KEY=key
 
 for LOCATION in $(cat $LIST)
 do
-	for FILE in $(find $LOCATION -type f -name '*.enc')
-	do
-        	openssl aes-128-cbc -salt -pass file:"$KEY" -in "$FILE" -out ${FILE%.*} -d && rm -rf "$FILE"
-	done
+        for FILE in $(find $LOCATION -type f -name '*.enc')
+        do
+                openssl aes-128-cbc -pbkdf2 -iter 100000 -salt -pass file:"$KEY" -in "$FILE" -out ${FILE%.*} -d && rm -rf "$FILE"
+        done
 done
 
